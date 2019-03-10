4f3f2df320d58683ea0c1ce1d11a1bb50724d6c9
HariPrasad.M <hariprasad.m@transneuron.com>
2019-03-10 22:40:15 +0530

    modifing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-// import './App.css';
-// import Joke from './Jock';
-// import jokesData from './jokesData';
-// import Product from './Product';
-// import vschoolProducts from './vschoolProducts';
-// import Todoitem from './Todoitem';
-// import todoData from './todoData'
+import React,{Component} from 'react';
+import './App.css';
+import Joke from './Jock';
+import jokesData from './jokesData';
+import Product from './Product';
+import vschoolProducts from './vschoolProducts';
+import Todoitem from './Todoitem';
+import todoData from './todoData'
 
 // https://developer.mozilla.org/en-US/docs/web/JavaScript/Reference/Global_objects/Arrary/filter
 
@@ -40,5 +40,86 @@ import React from 'react';
 // 		</div>
 // 	)
 // }
-class App extends Re
+
+// Second prog
+// class App extends React.Component{
+//   yourMethodHere(){
+
+//   }
+
+//   render(){
+//     const date = new Date();
+//     return(
+//         <div>
+//               <h1>{this.props.whatever}</h1>  
+//         </div>
+//     )
+//   }
+// }
+
+//Second prog
+
+// class App extends Component{
+//   constructor(){
+//     super()
+//     this.state ={
+//       answer:"Yes"
+//     }
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <h1>Is state important to know?{this.state.answer}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+//Third prog
+
+// class App extends Component{
+//   constructor(){
+//     super()
+//     this.state={
+//       name:"sridhar",
+//       age:25
+//     }
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <h1>{this.state.name}</h1>
+//         <h3>{this.state.age}years old</h3>
+//       </div>
+//     )
+//   }
+// }
+
+
+class App extends Component{
+  constructor(){
+    super()
+    this.state= {
+      toLoggedIn : true
+    }
+  }
+  render(){
+    let wordDisplay
+      if(this.state.toLoggedIn === true){
+        wordDisplay = "in"
+      } else {
+          wordDisplay = "out"
+      }
+
+    return(
+      <div>
+          <h1> You are currently logged{wordDisplay}</h1>
+      </div>
+
+    )
+  }
+
+}
+
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App'
+
+
+
+// class App extends React.Component{
+// 	render(){		
+// 		return(
+// 			<div>
+// 				<Header username="sridhar" />
+// 				<Greeting />
+// 			</div>
+// 		)
+// 	}
+// }
+
+
+
+// class Header extends React.Component{
+// 	render(){
+// 		return(
+// 			<header>
+// 			 	<p>welcome,{this.props.username} !</p>
+// 			</header>
+// 		)
+// 	}	
+// }
+
+
+// class Greeting extends React.Component{
+// 	render(){
+// 		const date = new Date()
+// 		const hours = date.getHours()
+// 		let timeOfDay
+
+// 		if(hours < 12){
+// 			timeOfDay ="Morning"
+// 		}else if (hours >=12 && hours <17){
+// 			timeOfDay = "afternoon"
+// 		}else{
+// 			timeOfDay = "night"
+// 		}
+// 		return(
+// 			<h1>Good {timeOfDay} to your,sir or madam!</h1>
+// 		) 
+// 	}
+	
+// }
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
